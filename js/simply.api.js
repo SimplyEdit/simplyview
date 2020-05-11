2fa00b0c8e9a88ef39ea8d9ef723e6d4f705ded4
Auke van Slooten <auke@muze.nl>
2020-05-12 00:20:43 +0200

    added callHandler, to allow you to change how/when fetch is actually called added explicit apply to resultHandler and errorHandler, so all handlers have the options object as this. any get access now sets the new proxy in the cache, repeated access returns that proxy this also allows you to set properties in the api, these will return whatever you set them to renamed cache.options to cache.$options, this prevents any possible name conflict with a remote api

--- js/simply.api.js
+++ js/simply.api.js
@@ -3,6 +3,9 @@
 
 
     var defaultOptions = {
+		/**
+         * called with the raw result of fetch. this is set to the options object.
+         */
         resultHandler: function(result) {
             if (result.ok) {
                 return result.json();
@@ -14,15 +17,25 @@
                 }
             }
         },
+        
         errorHandler: function(error) {
             console.error(error.status, error.message);
         },
+        /**
+         * called with the http verb to use. must return a function to fetch with that http verb
+         * this should by default use the verb method in this.verbs
+         * but may also do additional stuff, like refresh a token or login
+         * if so, it should return a function that returns a promise that runs the verb method
+         */
+		callHandler: function(verb) {
+			return this.verbs[verb];
+		},
         verbs: {
             get: function(params) {
-                return fetchApi('get',this, params);
+                return fetchApi('get', this, params);
             },
             post: function(params) {
-                return fetchApi('post',this,params);
+                return fetchApi('post', this, params);
             }
         }
     };
@@ -35,8 +48,21 @@
         return path+name;
     }
 
+	/**
+     * Fetches the options.baseURL using the fetch api and returns a promise
+     * Extra options in addition to those of global.fetch():
+     * - user (and password): if set, a basic authentication header will be added
+     * - formData: if true, params are sent as formData in the body
+     * - jsonData: if true, params are sent as json in the bodu
+     * By default params, if set, will be added to the baseURL as searchParams
+     * @param method one of the http verbs, e.g. get, post, etc.
+     * @param options the options for fetch(), with some additions
+     * @param params the parameters to send with the request, as javascript/json data
+     * @return Promise
+     */
     function fetchApi(method, options, params) {
         var url = new URL(options.baseURL);
+		var fetchOptions = Object.assign({}, options);
         if (params && options.formData) {
             var formData = new FormData();
             for (const name in params) {
@@ -51,23 +77,16 @@
             }
             url.search = searchParams.toString();
         }
-        var fetchOptions = {
-            method: method.toUpperCase()
-        };
         if (formData) {
             fetchOptions.body = formData
         }
-        if (options.cors) {
-            fetchOptions.mode = 'cors';
-        }
-        if (options.headers) {
-            fetchOptions.headers = options.headers;
-        } else {
+        if (!options.headers) {
             fetchOptions.headers = [];
         }
         if (options.user) {
             fetchOptions.headers.push('Authorization: Basic '+btoa(options.user+':'+options.password));
         }
+		fetchOptions.method = method.toUpperCase();
         var fetchURL = url.toString()
         return fetch(fetchURL, fetchOptions);
     }
@@ -76,24 +95,39 @@
         options = Object.assign(defaultOptions, options);
         return {
             get: function(cache, prop) {
-                if (Object.keys(options.verbs).indexOf(prop)!=-1) { // property matches one of the http verbs: get, post, etc.
-                    return function(params) {
-                        return options.verbs[prop]
-                            .apply(cache, params)
-                            .then(options.resultHandler)
-                            .catch(options.errorHandler);
+				if (cache[prop]) {
+					return cache[prop];
+				} else if (Object.keys(options.verbs).indexOf(prop)!=-1) { 
+					// property matches one of the http verbs: get, post, etc.
+                    cache[prop] = function(params) {
+                        return options.callHandler
+							.call(cache.$options, prop)
+                            .apply(cache.$options, params)
+                            .then(function(res) {
+								return options.resultHandler.call(cache.$options, res);
+							})
+                            .catch(function(err) {
+								return options.errorHandler.call(cache.$options, err);
+							});
                     }
+					return cache[prop];
                 } else {
-                    return api.create(Object.assign(options, {
+                    cache[prop] = api.create(Object.assign(options, {
                         baseURL: cd(options.baseURL, prop)
                     }));
+					return cache[prop];
                 }
             },
             apply: function(cache, thisArg, params) {
-                return options.verbs.get
-                    .apply(cache, params)
-                    .then(options.resultHandler)
-                    .catch(options.errorHandler);
+                return options.callHandler
+					.call(cache.$options, 'get')
+                    .apply(cache.$options, params)
+                    .then(function(res) {
+						return options.resultHandler.call(cache.$options, res);
+					})
+                    .catch(function(err) {
+						return options.errorHandler.call(cache.$options, err);
+					});
             }
         }
     }
@@ -101,7 +135,7 @@
     var api = {
         create: function(options) {
             var cache = () => {};
-            cache.options = options;
+            cache.$options = options;
             return new Proxy( cache, getApiHandler(options) );
         }
     }
