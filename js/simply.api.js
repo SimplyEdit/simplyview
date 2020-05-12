3d91d6b4e2e8706968582e00600150bc0e3edd25
Auke van Slooten <auke@muze.nl>
2020-05-12 11:08:07 +0200

    refactored the api to be easier to understand and use moved default handler methods to global simply.api, so they can be used standalone as well as wrapped in custom handlers

--- js/simply.api.js
+++ js/simply.api.js
@@ -1,144 +1,173 @@
 (function(global) {
     'use strict';
 
-
-    var defaultOptions = {
+    var api = {
 		/**
-         * called with the raw result of fetch. this is set to the options object.
+         * Returns a Proxy object that translates property access to a URL in the api
+         * and method calls to a fetch on that URL.
+         * @param options: a list of options for fetch(), 
+		 * see the 'init' parameter at https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch#parameters 
+         * additionally:
+         * - baseURL: (required) the endpoint of the API
+         * - path: the current path in the API, is appended to the baseURL
+         * - verbs: list of http verbs to allow as methods, default ['get','post']
+         * - handlers.fetch: alternative fetch method
+         * - handlers.result: alternative getResult method
+         * - handlers.error: alternative error method
+         * - user (and password): if set, a basic authentication header will be added
+         * - paramsFormat: either 'formData', 'json' or 'search'. Default is search.
+         * - responseFormat: test, formData, blob, json, arrayBuffer or unbuffered. Default is json.
+         * @return Proxy
          */
-        resultHandler: function(result) {
-            if (result.ok) {
-                return result.json();
-            } else {
-                throw {
-                    status: result.status,
-                    message: result.statusText,
-                    response: result
-                }
-            }
+        proxy: function(options) {
+            var cache = () => {};
+            cache.$options = options;
+            return new Proxy( cache, getApiHandler(options) );
         },
-        
-        errorHandler: function(error) {
-            console.error(error.status, error.message);
+
+        /**
+         * Fetches the options.baseURL using the fetch api and returns a promise
+         * Extra options in addition to those of global.fetch():
+         * - user (and password): if set, a basic authentication header will be added
+         * - paramsFormat: either 'formData', 'json' or 'search'
+         * By default params, if set, will be added to the baseURL as searchParams
+         * @param method one of the http verbs, e.g. get, post, etc.
+         * @param options the options for fetch(), with some additions
+         * @param params the parameters to send with the request, as javascript/json data
+         * @return Promise
+         */
+        fetch: function(method, params, options) {
+            var url = new URL(options.baseURL+options.path);
+            var fetchOptions = Object.assign({}, options);
+            if (params && options.paramsFormat == 'formData') {
+                var formData = new FormData();
+                for (const name in params) {
+                    formData.append(name, params[name]);
+                }
+            } else if (params && options.paramsFormat == 'json') {
+                var formData = params;
+            } else if (params && options.paramsFormat == 'search') {
+                var searchParams = url.searchParams; //new URLSearchParams(url.search.slice(1));
+                for (const name in params) {
+                    searchParams.set(name, params[name]);
+                }
+                url.search = searchParams.toString();
+            } else {
+				throw Error('Unknown options.paramsFormat '+options.paramsFormat+'. Select one of formData, json or search.');
+			}
+            if (formData) {
+                fetchOptions.body = formData
+            }
+            if (!options.headers) {
+                fetchOptions.headers = [];
+            }
+            if (options.user) {
+                fetchOptions.headers.push('Authorization: Basic '+btoa(options.user+':'+options.password));
+            }
+            fetchOptions.method = method.toUpperCase();
+            var fetchURL = url.toString()
+            return fetch(fetchURL, fetchOptions);
         },
         /**
-         * called with the http verb to use. must return a function to fetch with that http verb
-         * this should by default use the verb method in this.verbs
-         * but may also do additional stuff, like refresh a token or login
-         * if so, it should return a function that returns a promise that runs the verb method
+         * Handles the response and returns a Promise with the response data as specified
+         * @param response Response
+         * @param options
+         * - responseFormat: one of 'text', 'formData', 'blob', 'arrayBuffer', 'unbuffered' or 'json'.
+         * The default is json.
          */
-		callHandler: function(verb) {
-			return this.verbs[verb];
-		},
-        verbs: {
-            get: function(params) {
-                return fetchApi('get', this, params);
-            },
-            post: function(params) {
-                return fetchApi('post', this, params);
+		getResult: function(response, options) {
+            if (response.ok) {
+				switch(options.responseFormat) {
+					case 'text':
+						return response.text();
+					break;
+					case 'formData':
+						return response.formData();
+					break;
+					case 'blob':
+						return response.blob();
+					break;
+					case 'arrayBuffer':
+						return response.arrayBuffer();
+					break;
+					case 'unbuffered':
+						return response.body;
+					break;
+					case 'json':
+					default:
+		                return response.json();
+					break;
+				}
+            } else {
+                throw {
+                    status: response.status,
+                    message: response.statusText,
+                    response: response
+                }
             }
-        }
+		},
+		logError: function(error, options) {
+            console.error(error.status, error.message);
+		}
+    }
+
+    var defaultOptions = {
+		path: '',
+		responseFormat: 'json',
+		paramsFormat: 'search',
+		verbs: ['get','post'],
+		handlers: {
+			fetch:  api.fetch,
+			result: api.jsonResult,
+			error:  api.logError
+		}
     };
 
     function cd(path, name) {
         name = name.replace(/\//g,'');
-        if (!path[path.length-1]!='/') {
+        if (!path.length || !path[path.length-1]!='/') {
             path+='/';
         }
-        return path+name;
+        return path+encodeURIComponent(name);
     }
 
-	/**
-     * Fetches the options.baseURL using the fetch api and returns a promise
-     * Extra options in addition to those of global.fetch():
-     * - user (and password): if set, a basic authentication header will be added
-     * - formData: if true, params are sent as formData in the body
-     * - jsonData: if true, params are sent as json in the bodu
-     * By default params, if set, will be added to the baseURL as searchParams
-     * @param method one of the http verbs, e.g. get, post, etc.
-     * @param options the options for fetch(), with some additions
-     * @param params the parameters to send with the request, as javascript/json data
-     * @return Promise
-     */
-    function fetchApi(method, options, params) {
-        var url = new URL(options.baseURL);
-		var fetchOptions = Object.assign({}, options);
-        if (params && options.formData) {
-            var formData = new FormData();
-            for (const name in params) {
-                formData.append(name, params[name]);
-            }
-        } else if (params && options.jsonData) {
-            var formData = params;
-        } else if (params) {
-            var searchParams = url.searchParams; //new URLSearchParams(url.search.slice(1));
-            for (const name in params) {
-                searchParams.set(name, params[name]);
-            }
-            url.search = searchParams.toString();
-        }
-        if (formData) {
-            fetchOptions.body = formData
-        }
-        if (!options.headers) {
-            fetchOptions.headers = [];
-        }
-        if (options.user) {
-            fetchOptions.headers.push('Authorization: Basic '+btoa(options.user+':'+options.password));
-        }
-		fetchOptions.method = method.toUpperCase();
-        var fetchURL = url.toString()
-        return fetch(fetchURL, fetchOptions);
-    }
+	function fetchChain(prop, params) {
+		var options = this;
+		return this.handlers.fetch
+			.call(this, prop, params, options)
+			.then(function(res) {
+				return options.handlers.result.call(options, res, options);
+			})
+			.catch(function(error) {
+				return options.handlers.error.call(options, error, options);
+			});
+	}
 
     function getApiHandler(options) {
+		options.handlers = Object.assign(defaultOptions.handlers, options.handlers);
         options = Object.assign(defaultOptions, options);
+
         return {
             get: function(cache, prop) {
-				if (cache[prop]) {
-					return cache[prop];
-				} else if (Object.keys(options.verbs).indexOf(prop)!=-1) { 
-					// property matches one of the http verbs: get, post, etc.
-                    cache[prop] = function(params) {
-                        return options.callHandler
-							.call(cache.$options, prop)
-                            .apply(cache.$options, params)
-                            .then(function(res) {
-								return options.resultHandler.call(cache.$options, res);
-							})
-                            .catch(function(err) {
-								return options.errorHandler.call(cache.$options, err);
-							});
-                    }
-					return cache[prop];
+                if (cache[prop]) {
+                    return cache[prop];
+                } else if (options.verbs.indexOf(prop)!=-1) { 
+                    // property matches one of the http verbs: get, post, etc.
+                    cache[prop] = fetchChain.call(options, prop, params);
+                    return cache[prop];
                 } else {
-                    cache[prop] = api.create(Object.assign(options, {
-                        baseURL: cd(options.baseURL, prop)
+                    cache[prop] = api.proxy(Object.assign(options, {
+                        path: cd(options.path, prop)
                     }));
-					return cache[prop];
+                    return cache[prop];
                 }
             },
             apply: function(cache, thisArg, params) {
-                return options.callHandler
-					.call(cache.$options, 'get')
-                    .apply(cache.$options, params)
-                    .then(function(res) {
-						return options.resultHandler.call(cache.$options, res);
-					})
-                    .catch(function(err) {
-						return options.errorHandler.call(cache.$options, err);
-					});
+                return fetchChain.call(options, 'get', params)
             }
         }
     }
 
-    var api = {
-        create: function(options) {
-            var cache = () => {};
-            cache.$options = options;
-            return new Proxy( cache, getApiHandler(options) );
-        }
-    }
 
     if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
         module.exports = api;
