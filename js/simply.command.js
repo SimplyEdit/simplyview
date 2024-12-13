12c44f6056648f7bbb5bb3c6d191dcb326acd812
Yvo Brevoort <yvo@muze.nl>
2024-12-13 23:36:26 +0100

    remove default commands

--- js/simply.command.js
+++ js/simply.command.js
@@ -1,46 +1,6 @@
 (function(global) {
     'use strict';
 
-    var defaultCommands = {
-        'simply-hide': function(el, value) {
-            var target = this.app.get(value);
-            if (target) {
-                this.action('simply-hide',target);
-            }
-        },
-        'simply-show': function(el, value) {
-            var target = this.app.get(value);
-            if (target) {
-                this.action('simply-show',target);
-            }
-        },
-        'simply-select': function(el, value) {
-            var group = el.dataset.simplyGroup;
-            var target = this.app.get(value);
-            var targetGroup = (target ? target.dataset.simplyGroup : null);
-            this.action('simply-select', el, group, target, targetGroup);
-        },
-        'simply-toggle-select': function(el, value) {
-            var group = el.dataset.simplyGroup;
-            var target = this.app.get(value);
-            var targetGroup = (target ? target.dataset.simplyTarget : null);
-            this.action('simply-toggle-select',el,group,target,targetGroup);
-        },
-        'simply-toggle-class': function(el, value) {
-            var target = this.app.get(el.dataset.simplyTarget);
-            this.action('simply-toggle-class',el,value,target);
-        },
-        'simply-deselect': function(el, value) {
-            var target = this.app.get(value);
-            this.action('simply-deselect',el,target);
-        },
-        'simply-fullscreen': function(el, value) {
-            var target = this.app.get(value);
-            this.action('simply-fullscreen',target);
-        }
-    };
-
-
     var handlers = [
         {
             match: 'input,select,textarea',
@@ -135,7 +95,7 @@
 
     var command = function(app, inCommands) {
 
-        var commands = Object.create(defaultCommands);
+        var commands = Object.create();
         for (var i in inCommands) {
             commands[i] = inCommands[i];
         }
