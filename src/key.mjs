3a0b73a78966b3c4f03bc95cd703e57b8247ec96
Auke van Slooten <auke@muze.nl>
2024-12-24 14:19:13 +0100

    moved keyhandler to simplykey, so it can use this bind key functions to simply app initialize with empty keys if not specified

--- src/key.mjs
+++ src/key.mjs
@@ -1,13 +1,52 @@
 class SimplyKeys {
-	constructor(options = {} ) {
+	constructor(options = {}) {
 		if (!options.app) {
 			options.app = {}
 		}
 		if (!options.app.container) {
 			options.app.container = document.body
 		}
+		this.keys = options.keys || {}
 		this.app = options.app
-		this.app.container.addEventListener('keydown', keyHandler(this.keys))
+		this.app.container.addEventListener('keydown', this.keyHandler())
+	}
+
+	keyHandler() {
+		return (e) => {
+			if (e.isComposing || e.keyCode === 229) {
+			    return;
+			}
+			if (e.defaultPrevented) {
+			    return;
+			}
+			if (!e.target) {
+			    return;
+			}
+
+			let selectedKeyboard = 'default';
+			if (e.target.closest('[data-simply-keyboard]')) {
+			    selectedKeyboard = e.target.closest('[data-simply-keyboard]').dataset.simplyKeyboard;
+			}
+			let key = '';
+			if (e.ctrlKey && e.keyCode!=17) {
+			    key+='Control+';
+			}
+			if (e.metaKey && e.keyCode!=224) {
+			    key+='Meta+';
+			}
+			if (e.altKey && e.keyCode!=18) {
+			    key+='Alt+';
+			}
+			if (e.shiftKey && e.keyCode!=16) {
+			    key+='Shift+';
+			}
+			key+=e.key;
+
+			if (this.keys[selectedKeyboard] && this.keys[selectedKeyboard][key]) {
+			    let keyboard = this.keys[selectedKeyboard]
+			    keyboard[key].call(this.app,e);
+			}
+		}
 	}
 }
 
@@ -15,41 +54,3 @@ export function keys(options={}) {
 	return new SimplyKeys(options)
 }
 
-function keyHandler(keys) {
-	return (e) => {
-		if (e.isComposing || e.keyCode === 229) {
-		    return;
-		}
-		if (e.defaultPrevented) {
-		    return;
-		}
-		if (!e.target) {
-		    return;
-		}
-
-		let selectedKeyboard = 'default';
-		if (e.target.closest('[data-simply-keyboard]')) {
-		    selectedKeyboard = e.target.closest('[data-simply-keyboard]').dataset.simplyKeyboard;
-		}
-		let key = '';
-		if (e.ctrlKey && e.keyCode!=17) {
-		    key+='Control+';
-		}
-		if (e.metaKey && e.keyCode!=224) {
-		    key+='Meta+';
-		}
-		if (e.altKey && e.keyCode!=18) {
-		    key+='Alt+';
-		}
-		if (e.shiftKey && e.keyCode!=16) {
-		    key+='Shift+';
-		}
-		key+=e.key;
-
-		if (keys[selectedKeyboard] && keys[selectedKeyboard][key]) {
-		    let keyboard = keys[selectedKeyboard]
-		    keyboard.app = keys.app;
-		    keyboard[key].call(keys.app,e);
-		}
-	}
-}
