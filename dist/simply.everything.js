21c9325313b6f0ba7b5aed7e8bea7b0d2a1efeda
Auke van Slooten <auke@muze.nl>
2025-01-03 15:15:23 +0100

    rebuild

--- dist/simply.everything.js
+++ dist/simply.everything.js
@@ -932,7 +932,6 @@
      * Finds the first matching template and creates a new DocumentFragment
      * with the correct data bind attributes in it (prepends the current path)
      */
-<<<<<<< HEAD
     applyTemplate(context) {
       const path = context.path;
       const templates = context.templates;
@@ -941,10 +940,6 @@
       const parent = context.parent;
       const value = list ? list[index] : context.value;
       let template = this.findTemplate(templates, value);
-=======
-    applyTemplate(path, templates, list, index) {
-      let template = this.findTemplate(templates, list[index]);
->>>>>>> 9afe95a (updated build)
       if (!template) {
         let result = new DocumentFragment();
         result.innerHTML = "<!-- no matching template -->";
