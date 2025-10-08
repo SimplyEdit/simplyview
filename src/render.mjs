5deb13a61e9eb8a54b797fe53e73e6769153df4e
Auke van Slooten <auke@muze.nl>
2025-10-08 13:21:11 +0200

    added missing simply-render element

---
+++ src/render.mjs
@@ -0,0 +1,27 @@
+export class SimplyRender extends HTMLElement 
+{
+    constructor()
+    {
+        super()
+        let templateId = this.getAttribute("rel")
+        let template = document.getElementById(templateId)
+
+        if (template) {
+            let content = template.content.cloneNode(true)
+            for (const node of content.childNodes) {
+                const clone = node.cloneNode(true)
+                if (clone.nodeType == document.ELEMENT_NODE) {
+                    clone.querySelectorAll("template").forEach(function(t) {
+                        t.setAttribute("simply-render", "")
+                    })
+                }
+                this.parentNode.insertBefore(clone, this)
+            }
+            this.parentNode.removeChild(this)
+        }
+    }
+}
+
+if (!customElements.get('simply-render')) {
+    customElements.define('simply-render', SimplyRender);
+}
