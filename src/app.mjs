788807cf718f678fa3d6cf9d4ea0fb643ebb7a0d
Auke van Slooten <auke@muze.nl>
2025-10-14 09:45:52 +0200

    added root option to specify root url to use

--- src/app.mjs
+++ src/app.mjs
@@ -16,8 +16,14 @@ class SimplyApp {
 				case 'keyboard': // backwards compatible
 					this.keys = keys({ app: this, keys: options.keys })
 					break
+				case 'root':
+					this.root = options.root
+					break
 				case 'routes':
 					this.routes = routes({ app: this, routes: options.routes})
+					if (this.root) {
+						this.routes.init({ root: this.root })
+					}
 					this.routes.handleEvents();
 					globalThis.setTimeout(() => {
 						if (this.routes.has(globalThis.location?.hash)) {
