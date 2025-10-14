030ec4b10c1c3730ec00675aa2ad57b9ea633138
Auke van Slooten <auke@muze.nl>
2025-10-14 09:46:10 +0200

    added feature to return a destroy function in an activate listener, which gets called whenever an activated element leaves the dom

--- src/activate.mjs
+++ src/activate.mjs
@@ -1,3 +1,7 @@
+if (!Symbol.onDestroy) {
+    Symbol.onDestroy = Symbol('onDestroy')
+}
+
 const listeners = new Map()
 
 export const activate = {
@@ -31,7 +35,12 @@ function callListeners(node) {
     const activate = node?.dataset?.simplyActivate
     if (activate && listeners.has(activate)) {
         for (let callback of listeners.get(activate)) {
-            callback.call(node)
+            const onDestroy = callback.call(node)
+            if (typeof onDestroy == 'function') {
+                node[Symbol.onDestroy] = onDestroy
+            } else if (typeof onDestroy != 'undefined') {
+                console.warn('activate listener may only return a de-activate function, instead got', onDestroy)
+            }
         }
     }
 }
@@ -42,11 +51,24 @@ function handleChanges(changes) {
         if (change.type == 'childList') {
             for (let node of change.addedNodes) {
                 if (node.querySelectorAll) {
-                    var toActivate = Array.from(node.querySelectorAll('[data-simply-activate]'))
+                    let toActivate = Array.from(node.querySelectorAll('[data-simply-activate]'))
                     if (node.matches('[data-simply-activate]')) {
                         toActivate.push(node)
                     }
                     activateNodes = activateNodes.concat(toActivate)
+                }
+            }
+            for (let node of change.removedNodes) {
+                if (node.querySelectorAll) {
+                    let toDestroy = Array.from(node.querySelectorAll('[data-simply-activate]'))
+                    if (node.matches['[data-simply-activate']) {
+                        toDestroy.push(node)
+                    }
+                    for (let child of toDestroy) {
+                        if (child[Symbol.onDestroy]) {
+                            child[Symbol.onDestroy].call(child)
+                        }
+                    }
                 }
             }
         }
