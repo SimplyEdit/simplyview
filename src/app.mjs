7bab9f592ff54bfab292e6ab128c2b1ae0e0068b
Auke van Slooten <auke@muze.nl>
2025-12-14 15:02:47 +0100

    added components, subcomponents, html and css options added html and css templated literals (need better place and checks)

--- src/app.mjs
+++ src/app.mjs
@@ -4,11 +4,65 @@ import { actions } from './action.mjs'
 import { keys } from './key.mjs'
 import { view } from './view.mjs'
 
-class SimplyApp {
-	constructor(options={}) {
+class SimplyApp
+{
+
+	constructor(options={})
+	{
 		this.container = options.container || document.body
+		if (options.components) {
+			this.mergeComponents(options, options.components)
+		}
+		this.initOptions(options)
+	}
+
+	get app()
+	{
+		return this
+	}
+
+	async start()
+	{
+		if (this.hooks?.start) {
+			await this.hooks.start()
+		}
+		if (this.routes) {
+			if (this.baseURL) {
+				this.routes.init({ baseURL: this.baseURL })
+			}
+			this.routes.handleEvents();
+			globalThis.setTimeout(() => {
+				if (this.routes.has(globalThis.location?.hash)) {
+					this.routes.match(globalThis.location.hash)
+				} else {
+					this.routes.match(globalThis.location?.pathname+globalThis.location?.hash)
+				}
+			});
+		}
+	}
+
+	initOptions(options)
+	{
 		for (let key in options) {
 			switch(key) {
+				case 'html':
+					for (const name in options.html) {
+						const template = document.createElement('template')
+						template.id=name
+						const element = document.createElement('div')
+						element.innerHTML = options.html[name]
+						template.content.append(...element.children)
+						this.container.appendChild(template)
+					}
+				break
+				case 'css':
+					for (const name in options.css) {
+						const style = document.createElement('style')
+						style.id=name 
+						style.innerText = options.css[name]
+						this.container.appendChild(style)
+					}
+				break
 				case 'commands':
 					this.commands = commands({ app: this, container: this.container, commands: options.commands})
 					break
@@ -58,6 +112,9 @@ class SimplyApp {
 					}
 					this[key] = new Proxy(options[key], moduleHandler)
 					break
+				components:
+					this.components = components
+					break
 				default:
 					console.log('simply.app: unknown initialization option "'+key+'", added as-is')
 					this[key] = options[key]
@@ -65,25 +122,48 @@ class SimplyApp {
 			}
 		}
 	}
-	get app() {
-		return this
-	}
-	async start() {
-		if (this.hooks?.start) {
-			await this.hooks.start()
-		}
-		if (this.routes) {
-			if (this.baseURL) {
-				this.routes.init({ baseURL: this.baseURL })
+
+	mergeOptions(options, otherOptions)
+	{
+		for (const key in otherOptions) {
+			switch(typeof otherOptions[key]) {
+				case 'object':
+					if (!otherOptions[key]) {
+						continue // null
+					}
+					if (!options[key]) {
+						options[key] = otherOptions[key]
+					} else {
+						//FIXME: check that options[key] is also an object
+						this.mergeOptions(options[key], otherOptions[key])
+					}
+					break
+				default:
+					options[key] = otherOptions[key]
 			}
-			this.routes.handleEvents();
-			globalThis.setTimeout(() => {
-				if (this.routes.has(globalThis.location?.hash)) {
-					this.routes.match(globalThis.location.hash)
-				} else {
-					this.routes.match(globalThis.location?.pathname+globalThis.location?.hash)
+		}
+	}
+
+	mergeComponents(options, components) {
+		for (const name in components) {
+			const component = components[name]
+			if (component.components) {
+				this.mergeComponents(options, component.components)
+			}
+			options.components[name] = component
+			for (const key in component) {
+				switch(key) {
+					case 'components':
+						// already handled
+						break
+					default:
+						if (!options[key]) {
+							options[key] = Object.create(null)
+						}
+						this.mergeOptions(options[key], component[key])
+						break
 				}
-			});
+			}
 		}
 	}
 }
--- src/everything.mjs
+++ src/everything.mjs
@@ -19,6 +19,34 @@ const simply = {
 	view
 }
 
-globalThis.simply = simply
+function escapeHTML(content) {
+	return (''+content)
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#39;')
+}
 
+function escapeCSS(content) {
+	// don't allow </style>
+	return (''+content)
+		.replace(/<\//g, '<')
+}
+
+globalThis.simply = simply
+globalThis.html = function(strings, ...values) {
+  const outputArray = values.map(
+    (value, index) =>
+      `${strings[index]}${escapeHTML(value)}`,
+  );
+  return outputArray.join("") + strings[strings.length - 1];
+}
+globalThis.css = function(strings, ...values) {
+  const outputArray = values.map(
+    (value, index) =>
+      `${strings[index]}${escapeCSS(value)}`,
+  );
+  return outputArray.join("") + strings[strings.length - 1];
+}
 export default simply
