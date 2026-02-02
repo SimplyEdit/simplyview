31f632b0beb9a13ca08b68b1e2d69554d5132bfb
Auke van Slooten <auke@muze.nl>
2026-02-02 11:30:49 +0100

    removed wait handler as a yagni

--- src/action.mjs
+++ src/action.mjs
@@ -7,46 +7,19 @@ export function actions(options, optionsCompat)
 	}
 
 	if (options.app) {
-		const waitHandler = {
-			apply(target, thisArg, argumentsList)
-			{
-				try {
-					const result = target(...argumentsList)
-					if (result instanceof Promise) {
-						options.app.hooks.wait(true)
-						return result.finally(() => {
-							options.app.hooks.wait(false, target)
-						})
-					}
-					return result
-				} catch(err) {
-				}
-			}
-		}
-
 		const functionHandler = {
 			apply(target, thisArg, argumentsList)
 			{
 				try {
 					const result = target(...argumentsList)
 					if (result instanceof Promise) {
-						if (options.app.hooks.wait) {
-							options.app.hooks.wait(true, target)
-							return result.catch(err => {
-								return options.app.hooks.error(err, target)
-							})
-							.finally(() => {
-								options.app.hooks.wait(false, target)
-							})
-						} else {
-							return result.catch(err => {
-								return options.app.hooks.error(err, target)
-							})							
-						}
+						return result.catch(err => {
+							return options.app.hooks.error.call(this, err, target)
+						})							
 					}
 					return result
 				} catch(err) {
-					return options.app.hooks.error(err, target)
+					return options.app.hooks.error.call(this, err, target)
 				}
 			}
 		}
@@ -59,8 +32,6 @@ export function actions(options, optionsCompat)
 				}
 				if (options.app.hooks?.error) {
 					return new Proxy(target[property].bind(options.app), functionHandler)
-				} else if (options.app.hooks?.wait) {
-					return new Proxy(target[property].bind(options.app), waitHandler)
 				} else {
 					return target[property].bind(options.app)
 				}
