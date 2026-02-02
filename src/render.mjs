d2dc0749ed2136f8a9155152c219120016bef407
Auke van Slooten <auke@muze.nl>
2026-02-02 10:47:33 +0100

    optimized simply-render to only add mutation observer if needed, and remove it after

--- src/render.mjs
+++ src/render.mjs
@@ -16,12 +16,35 @@ export class SimplyRender extends HTMLElement
                 const clone = node.cloneNode(true)
                 if (clone.nodeType == document.ELEMENT_NODE) {
                     clone.querySelectorAll("template").forEach(function(t) {
-                        t.setAttribute("simply-render", "")
+                        t.setAttribute("simply-render", "") //FIXME: whats this?
                     })
+                    if (this.attributes) {
+                        for (const attr of this.attributes) {
+                            if (attr.name!='rel') {
+                                clone.setAttribute(attr.name, attr.value)
+                            }
+                        }
+                    }
                 }
                 this.parentNode.insertBefore(clone, this)
             }
             this.parentNode.removeChild(this)
+        } else {
+            const observe = () => {
+                const observer = new MutationObserver(() => {
+                    template = document.getElementById(templateId)
+                    if (template) {
+                        observer.disconnect()
+                        this.replaceWith(this) // trigger connectedCallback?
+                    }
+                })
+                observer.observe(globalThis.document, {
+                    subtree: true,
+                    childList: true,
+                })
+            }
+
+            observe()
         }
     }
 }
@@ -29,22 +52,3 @@ export class SimplyRender extends HTMLElement
 if (!customElements.get('simply-render')) {
     customElements.define('simply-render', SimplyRender);
 }
-
-const handleChanges = () => {
-    const simplyrenders = globalThis.document.querySelectorAll('simply-render[rel]')
-    for (el of simplyrenders) {
-        if (document.querySelector('template#'+el.getAttribute('rel'))) {
-            el.replaceWith(el) // trigger connectedCallback?
-        }
-    }
-}
-
-const observe = () => {
-    observer = new MutationObserver(handleChanges)
-    observer.observe(globalThis.document, {
-        subtree: true,
-        childList: true,
-    })
-}
-
-observe()
